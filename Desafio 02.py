d7a94cd14acc5b7cd0787d3aae7998ff3a5c97d9
Georgia Cordeiro <ra99050@uem.br>
2021-08-13 09:15:04 -0300

    teste

---
+++ Desafio 02.py
@@ -0,0 +1,4 @@
+dia=input('Qual o dia do seu aniversário?')
+mes=input('E qual o mês?')
+ano=input('Que ano novê nasceu?')
+print('Você nasceu no dia '+ dia + 'de '+mes+' de '+ano+'. Correto?')
---
+++ Desafio 03.py
@@ -0,0 +1,4 @@
+soma= (num1=input('Digite o primeiro número: ')+ num2=input('Digite o segundo número: ')
+print('A soma entre eles é: ', soma ))
+
+
